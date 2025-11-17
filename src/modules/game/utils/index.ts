c20c34445dfd386fb80bf69cc1828995fbd9b948
Solswinters <solswinters@users.noreply.github.com>
2025-11-17 20:47:49 +0100

    feat: add game utils index

---
+++ src/modules/game/utils/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Game utilities exports
+ */
+
+export * from './collision-detection'
+export * from './game-physics'
+export * from './obstacle-generator'
+export * from './score-calculator'
+export * from './difficulty-scaler'
