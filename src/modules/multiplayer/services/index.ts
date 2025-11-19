71b54f6b35a6c35f6d0331d98c15064bc11b0029
Solswinters <solswinters@users.noreply.github.com>
2025-11-19 18:09:50 +0100

    feat: add multiplayer barrel export files
    - Create hooks barrel exports
    - Add components barrel exports
    - Implement services barrel exports
    - Consolidate multiplayer module structure
    - Improve import ergonomics

--- src/modules/multiplayer/services/index.ts
+++ src/modules/multiplayer/services/index.ts
@@ -1,8 +1,21 @@
 /**
- * Multiplayer services exports
+ * Multiplayer services barrel exports
  */
 
-export * from './ConnectionManager'
-export * from './RoomManager'
-export * from './MatchmakingService'
-export * from './SyncService'
+export { WebSocketService } from './WebSocketService'
+export { RoomService } from './RoomService'
+export { ChatService } from './ChatService'
+export { PresenceService } from './PresenceService'
+export { SyncService } from './SyncService'
+export { LatencyService } from './LatencyService'
+export { MatchmakingService } from './MatchmakingService'
+export { TeamService } from './TeamService'
+export { InviteService } from './InviteService'
+export { SpectatorService } from './SpectatorService'
+export { LeaderboardService } from './LeaderboardService'
+export { StatsService } from './StatsService'
+export { ReplayService } from './ReplayService'
+export { ConnectionRecoveryService } from './ConnectionRecoveryService'
+export { EmoteService } from './EmoteService'
+export { SessionService } from './SessionService'
+export { AnalyticsService } from './AnalyticsService'
