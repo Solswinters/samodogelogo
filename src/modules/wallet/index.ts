ad32121f5f73b5460d0cfb3331b6859a32e149ff
Solswinters <solswinters@users.noreply.github.com>
2025-11-19 13:46:38 +0100

    feat: add wallet module barrel exports
    - Create comprehensive wallet module exports
    - Export all components, hooks, and services
    - Include type definitions
    - Add utilities and store exports
    - Enable clean imports from wallet module

--- src/modules/wallet/index.ts
+++ src/modules/wallet/index.ts
@@ -2,7 +2,39 @@
  * Wallet module exports
  */
 
-export * from './components'
-export * from './hooks'
-export * from './services'
-export * from './utils'
+// Components
+export { WalletButton } from './components/WalletButton'
+export { NetworkSwitch } from './components/NetworkSwitch'
+export { TokenDisplay } from './components/TokenDisplay'
+export { RewardsDisplay } from './components/RewardsDisplay'
+export { TransactionList } from './components/TransactionList'
+
+// Hooks
+export { useTokenBalance } from './hooks/useTokenBalance'
+export { useTokenApproval } from './hooks/useTokenApproval'
+export { useRewards } from './hooks/useRewards'
+export { useTransactionHistory } from './hooks/useTransactionHistory'
+export { useENSAddress, useENSName, useENSAvatar } from './hooks/useENS'
+export { useWalletStore } from './hooks/useWalletStore'
+
+// Services
+export { contractService } from './services/ContractService'
+export { ensService } from './services/ENSService'
+export { RewardsService } from './services/RewardsService'
+
+// Types
+export type * from './types/blockchain'
+
+// Utils
+export * from './utils/blockchain'
+export * from './utils/format'
+
+// Stores
+export { useTransactionsStore } from './stores/transactions-store'
+
+// ABIs
+export { GameTokenABI } from './abi/GameToken'
+export { GameRewardsABI } from './abi/GameRewards'
+
+// Config
+export { config } from './config/web3'
